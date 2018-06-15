f29c6d8e3f4c194757a9f2920e9d0b9fc61c554a
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2018-06-15 09:46:26 +0200

    update files tf

--- terraform/terraform-boostrap.tf
+++ terraform/terraform-boostrap.tf
@@ -2,7 +2,7 @@ module "bootstrap-training" {
   MOD_JSON_PATH       = "sandbox-wescale.json"
   MOD_PROJECT         = "sandbox-wescale"
   MOD_REGION          = "europe-west1"
-  MOD_COUNT           = 5 
+  MOD_COUNT           = 11 
 
   source = "modules"
 }
---
+++ traefik-ic/README.md
@@ -0,0 +1,2 @@
+
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=sebastien.lavayssiere@wescale.fr

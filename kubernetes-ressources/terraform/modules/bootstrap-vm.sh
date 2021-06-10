fbc620162799b67748168e9f41a3ea7193595090
stephane teyssier <stephane.teyssier@gmail.com>
2021-06-10 17:42:01 +0200

    Ubuntu 2010 (#30)
    * Ubuntu-2010 for bastion
    
    * Ubuntu-2010 for bastion - no gcloud
    
    * Ubuntu-2010 for bastion - kubectl
    
    * RM useless bootstrap.sh
    
    Co-authored-by: Stephane TEYSSIER <stephane.teyssier@wescale.fr>

--- kubernetes-ressources/terraform/modules/bootstrap-vm.sh
+++ kubernetes-ressources/terraform/modules/bootstrap-vm.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# Create an environment variable for the correct distribution
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 
 # Add the Cloud SDK distribution URI as a package source
-echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
 # Import the Google Cloud Platform public key
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 # Update the package list and install the Cloud SDK
-apt-get update && sudo apt-get install -y google-cloud-sdk kubectl nano unzip git
+apt-get update && sudo apt-get install -y kubectl nano unzip git
 
 # Install nodejs 10.X
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -

597869ef71c6e08e99b281c2a58ba1b4f67176ff
Mathieu LAUDE <mathieu.laude@wescale.fr>
2022-06-22 20:32:27 +0200

    Upgrade bastion to ubuntu 22.04 (fix)

--- kubernetes-ressources/terraform/modules/bootstrap-vm.sh
+++ kubernetes-ressources/terraform/modules/bootstrap-vm.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # Add the Cloud SDK distribution URI as a package source
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
 # Import the Google Cloud Platform public key
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
 
 # Update the package list and install the Cloud SDK
-apt-get update && sudo apt-get install -y kubectl nano unzip git google-cloud-sdk-gke-gcloud-auth-plugin
+apt-get update && sudo apt-get install -y kubectl nano unzip git xsel jq google-cloud-sdk-gke-gcloud-auth-plugin
+
+# Remove nodejs 12.X
+apt-get remove nodejs
+apt autoremove
 
 # Install nodejs 14.X
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -y nodejs
 
-apt-get install -y xsel jq
 npm i -g create-react-app
 
 gcloud config set compute/zone europe-west1-b
